1d20c18c3a4fd5bb1bb5b35a900ded01a9ff3271
Peter Odeny <odenypeter@gmail.com>
2017-06-29 12:28:39 +0300

    enhance the TolaUser serializer

--- userprofile/serializers.py
+++ userprofile/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import update_session_auth_hash
 from rest_framework import serializers
 from models import *
 
@@ -15,9 +16,46 @@ class CountrySerializer(serializers.HyperlinkedModelSerializer):
 		fields = '__all__'
 		
 
-class TolaUserSerialer(serializers.HyperlinkedModelSerializer):
+class TolaUserSerialer(serializers.ModelSerializer):
+    	
+    password = serializers.CharField(write_only=True, required=True)
+    confirm_password = serializers.CharField(write_only=True, required=True)
 
 	class  meta:
 		model = TolaUser
-		fields = '__all__'
+		fields = (
+            'id', 'email', 'username', 'date_created', 'date_modified',
+            'firstname', 'lastname', 'password', 'confirm_password')
+
+        read_only_fields = ('date_created', 'date_modified')
+
+		def create(self, validated_data):
+        return Account.objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get('email', instance.email)
+        instance.username = validated_data.get('username',instance.username)
+        instance.firstname = validated_data.get('firstname',instance.firstname)
+        instance.lastname = validated_data.get('lastname',instance.lastname)
+
+        password = validated_data.get('password', None)
+        confirm_password = validated_data.get('confirm_password', None)
+
+        if password and password == confirm_password:
+            instance.set_password(password)
+
+        instance.save()
+        return instance
+
+    def validate(self, data):
+        '''
+        Compare the passwords to ensure that they are same
+        '''
+        if data['password']:
+            if data['password'] != data['confirm_password']:
+                raise serializers.ValidationError(
+                    "The passwords must be the same"
+                )
+        return data
+
 
