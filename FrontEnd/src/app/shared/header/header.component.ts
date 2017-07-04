9b776ae45be8f8b9674f6420a566496ca250be04
Peter Odeny <odenypeter@gmail.com>
2017-07-04 12:54:50 +0300

    adjust the header to pick logged in username

--- FrontEnd/src/app/shared/header/header.component.ts
+++ FrontEnd/src/app/shared/header/header.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {UserprofileService} from 'app/userprofile/userprofile.service';
 
 @Component({
   selector: 'app-header',
+  providers: [UserprofileService],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  user;
+  constructor(private _service: UserprofileService ) { }
 
   ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem('loggedUser'));
+    console.log(this.user.user.username);
   }
 
+  logout(){
+    this._service.logout();
+
+  }
 }
