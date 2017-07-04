cc8621ce1e83ce1ca6cb02184ec9d82043621140
Peter Odeny <odenypeter@gmail.com>
2017-07-04 16:14:55 +0300

    add task Django REST  backend

--- task/views.py
+++ task/views.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from rest_framework import viewsets
+from models import *
+from serializers import *
 
 # Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
