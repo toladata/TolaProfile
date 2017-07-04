e5df5b731badab50abf88fb7692df03815ccd2c9
Peter Odeny <odenypeter@gmail.com>
2017-07-04 16:14:17 +0300

    add API urls

--- source/urls.py
+++ source/urls.py
@@ -1,7 +1,17 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework import routers
+from task.views import *
+
+router = routers.DefaultRouter()
+
+router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/auth/', include('userprofile.urls')),
+
+    #rest framework
+    url(r'^api/', include(router.urls)),
+
 ]
