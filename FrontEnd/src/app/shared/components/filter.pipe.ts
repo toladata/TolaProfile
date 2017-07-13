0d31bfca883697bc963c10665f859a7bb7227a08
Peter Odeny <odenypeter@gmail.com>
2017-07-13 13:00:43 +0300

    Add filter pipe

---
+++ FrontEnd/src/app/shared/components/filter.pipe.ts
@@ -0,0 +1,15 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+    name: 'itemsFilter',
+    pure: false
+})
+export class FilterPipe implements PipeTransform {
+  transform(allItems: any, searchText: any): any {
+    if(searchText == null) return allItems;
+
+    return allItems.filter(function(item){
+      return item.task.toLowerCase().indexOf(searchText.toLowerCase()) > -1;
+    })
+  }
+}
