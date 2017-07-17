56f2160f819d35637dd2091f15175fc8d20ecf93
Peter Odeny <odenypeter@gmail.com>
2017-07-17 11:43:01 +0300

    Move tasks code to the pages folder to conform with the new layout

---
+++ FrontEnd/src/app/pages/task/task.service.ts
@@ -0,0 +1,86 @@
+import {Injectable} from '@angular/core';
+import { Headers, RequestOptions,Response, Http} from '@angular/http';
+import { Observable } from "rxjs/Observable";
+import {AuthHttp, JwtHelper} from 'angular2-jwt';
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/observable/throw';
+
+const TOLAPOFILE_TASK_SERVER = 'http://127.0.0.1:8000/api/tasks/';
+const headers = new Headers({ 'Accept': 'application/json', 'Content-Type': 'application/json',});
+const  options = new RequestOptions({ headers : headers,});
+
+
+@Injectable()
+export class TaskService{
+    constructor(private http: Http, private _authHttp: AuthHttp) { }
+    getTasks(){
+        let jwtHelper: JwtHelper = new JwtHelper();
+
+        var token = localStorage.getItem('id_token');
+        console.log(
+            jwtHelper.decodeToken(token),
+            jwtHelper.getTokenExpirationDate(token),
+            jwtHelper.isTokenExpired(token)
+        );
+        return this._authHttp.get(TOLAPOFILE_TASK_SERVER, options)
+        .map(function(response){
+            return response.json();
+        });
+    }
+
+    createTask(taskData){
+
+        let postData = {
+                            "task": taskData.task,
+                            "priority": Number(taskData.priority),
+                            "status": 1,
+                            "submitter_email": taskData.submitter_email,
+                            "created_by": Number(taskData.created_by),
+                            "note": taskData.note
+                        };
+
+         return this._authHttp.post(TOLAPOFILE_TASK_SERVER, JSON.stringify(postData), options) 
+            .map(function(res){
+                return res.json();
+            })
+            .catch((error:any) => Observable.throw(error.json().error || 'Server error'))
+    }
+
+    taskDetails(task_id){
+        
+        return this._authHttp.get(TOLAPOFILE_TASK_SERVER+Number(task_id)+'/', options)
+            .map(function(response){
+                return response.json()}
+            )
+            .catch((error:any)=>Observable.throw(error.json().error || 'Server error'))
+    }
+
+    updateTask(task_id, editTaskData){
+
+         let postEditData = {
+                            "task": editTaskData.task,
+                            "priority": Number(editTaskData.priority),
+                            "status": Number(editTaskData.status),
+                            "submitter_email": editTaskData.submitter_email,
+                            "created_by": Number(editTaskData.created_by),
+                            "note": editTaskData.note,
+                            "assigned_to": Number(editTaskData.assigned_to),
+                            "due_date": new Date(editTaskData.due_date)
+                        };
+
+        return this._authHttp.put(TOLAPOFILE_TASK_SERVER+Number(task_id)+'/', JSON.stringify(postEditData), options)
+            .map(function(response){
+                return response.json()})
+            .catch((error:any)=>Observable.throw(error.json().error || 'Server error'))
+            
+    }
+
+    deleteTask(task_id){
+
+        return this._authHttp.delete(TOLAPOFILE_TASK_SERVER+Number(task_id)+'/', options)
+            .map(res=>res.json())
+            .catch((error:any)=>Observable.throw(error.json().error || 'Server error'))
+            .subscribe();
+    }
+}
