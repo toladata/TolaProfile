b733796adc03c10411e085896ee0606ba83fd275
Peter Odeny <odenypeter@gmail.com>
2017-07-17 11:45:31 +0300

    Update app module

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,17 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import {ReactiveFormsModule,FormsModule} from '@angular/forms';
 import { HttpModule, Http, RequestOptions } from '@angular/http';
-import {RoutingModule} from './router/router.module';
 import {RouterModule, Routes} from '@angular/router';
-import {TaskModule} from './task/task.module';
+import {Routing} from './app.routing';
 import {AuthConfig, AuthHttp } from 'angular2-jwt';
-
+import { PagesModule } from './pages/pages.module';
+import { LayoutModule } from "./layout/layout.module";
+import { UiComponentsModule } from './ui-components/ui-components.module';
+import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 import { LandingpageComponent } from "./landingpage.component";
-import { HeaderComponent } from './shared/header/header.component';
 import {AuthGuard} from './auth-guard';
 import {SharedService} from './shared/services/shared.service';
+import {ListComponent} from './pages/list/list.component';
 
 export function authHttpServiceFactory(http: Http, options: RequestOptions) {
   return new AuthHttp( new AuthConfig({headerPrefix: 'JWT'}), http, options);
@@ -20,17 +23,20 @@ export function authHttpServiceFactory(http: Http, options: RequestOptions) {
 @NgModule({
   declarations: [
     AppComponent,
+    ListComponent,
     LandingpageComponent,
-    HeaderComponent,
+
   ],
   imports: [
-    RoutingModule,
+    CommonModule,
+    SharedModule,
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpModule,
-    TaskModule,
-    
+    PagesModule,
+    LayoutModule,
+    UiComponentsModule,
+    Routing,
+    Routing,
+
   ],
   providers: [
     {
