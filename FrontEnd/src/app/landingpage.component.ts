b03e853f10515514303b605255e037eecf474319
gichiraanne <gichiraanne@gmail.com>
2017-07-24 07:34:11 +0300

    Adjustment of the landing page to use material forms

--- FrontEnd/src/app/landingpage.component.ts
+++ FrontEnd/src/app/landingpage.component.ts
@@ -7,7 +7,7 @@ import { Router } from "@angular/router";
 @Component({
   selector: 'app-landing',
   templateUrl: './landingpage.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class LandingpageComponent implements OnInit {
 
