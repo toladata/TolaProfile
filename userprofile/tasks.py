14e1fb10ca1a57f9447403a1badb59f6c18aec65
Peter Odeny <odenypeter@gmail.com>
2017-07-25 13:17:04 +0300

    Add tasks for synchronizing user data updates

--- userprofile/tasks.py
+++ userprofile/tasks.py
@@ -4,10 +4,10 @@ from celery import shared_task
 import requests
 import urlparse
 
-#sync data between TolaProfile and other TolaTools
+#sync with TolaWork
 @shared_task
-def sync_with_tolawork(data, url){
-    #construt TolaWork url
+def sync_create_with_tolawork(data, url){
+
     server_name = "tola.work"
     tola_work_url = urlparse.urljoin(url, server_name)
 
@@ -23,7 +23,25 @@ def sync_with_tolawork(data, url){
 }
 
 @shared_task
-def sync_with_activity(data, url){
+def sync_update_with_tolawork(data, url){
+
+    server_name = "tola.work"
+    tola_work_url = urlparse.urljoin(url, server_name)
+
+    token = ''
+    header = {'Authorization': 'token %s' % token}
+
+    api_response = requests.post(tola_work_url,json.dumps(data),headers=header)
+
+    if api_response.status_code == 200 || 201:
+        return True,
+    return false
+
+}
+
+#sync with TolaActivity
+@shared_task
+def sync_create_with_activity(data, url){
 
     token = ''
     header = {'Authorization': 'token %s' % token}
@@ -35,7 +53,31 @@ def sync_with_activity(data, url){
 }
 
 @shared_task
-def sync_with_table(data, url){
+def sync_update_with_activity(data, url){
+
+    token = ''
+    header = {'Authorization': 'token %s' % token}
+    api_response = requests.post(url,json.dumps(data),headers=header)
+
+    if api_response.status_code == 200 || 201:
+        return True,
+    return false
+}
+
+#sync with TolaTables
+@shared_task
+def sync_create_with_table(data, url){
+    token = ''
+    header = {'Authorization': 'token %s' % token}
+    api_response = requests.post(url,json.dumps(data),headers=header)
+
+    if api_response.status_code == 200 || 201:
+        return True,
+    return false
+}
+
+@shared_task
+def sync_update_with_table(data, url){
     token = ''
     header = {'Authorization': 'token %s' % token}
     api_response = requests.post(url,json.dumps(data),headers=header)
