8fe461de742684f2717ab58d6e6eee82ef70638a
Peter Odeny <odenypeter@gmail.com>
2017-07-25 16:52:13 +0300

    add task code to sync user deletion with the other tools

--- userprofile/tasks.py
+++ userprofile/tasks.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 import requests
 import urlparse
+import json
 
 #sync with TolaWork
 @shared_task
@@ -39,6 +40,23 @@ def sync_update_with_tolawork(data, url){
 
 }
 
+@shared_task
+def sync_delete_with_tolawork(data, url){
+
+    server_name = "tola.work"
+    tola_work_url = urlparse.urljoin(url+"/", server_name)
+
+    token = ''
+    header = {'Authorization': 'token %s' % token}
+
+    api_response = requests.delete(tola_work_url,json.dumps(data),headers=header)
+
+    if api_response.status_code == 200 || 201:
+        return True,
+    return false
+
+}
+
 #sync with TolaActivity
 @shared_task
 def sync_create_with_activity(data, url){
@@ -64,6 +82,18 @@ def sync_update_with_activity(data, url){
     return false
 }
 
+@shared_task
+def sync_delete_with_activity(data, url){
+
+    token = ''
+    header = {'Authorization': 'token %s' % token}
+    api_response = requests.delete(url,json.dumps(data),headers=header)
+
+    if api_response.status_code == 200 || 201:
+        return True,
+    return false
+}
+
 #sync with TolaTables
 @shared_task
 def sync_create_with_table(data, url){
@@ -77,10 +107,10 @@ def sync_create_with_table(data, url){
 }
 
 @shared_task
-def sync_update_with_table(data, url){
+def sync_delete_with_table(data, url){
     token = ''
     header = {'Authorization': 'token %s' % token}
-    api_response = requests.post(url,json.dumps(data),headers=header)
+    api_response = requests.delete(url,json.dumps(data),headers=header)
 
     if api_response.status_code == 200 || 201:
         return True,
