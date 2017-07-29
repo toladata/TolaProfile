9822f67774a006044f9dc64d6e503f40cc1ffe5e
Peter Odeny <odenypeter@gmail.com>
2017-07-29 11:29:11 +0300

    Keep track of the dislodged variable to adjust the nabber without page reload on login and logout

--- FrontEnd/src/app/pages/userprofile/userprofile.service.ts
+++ FrontEnd/src/app/pages/userprofile/userprofile.service.ts
@@ -1,17 +1,21 @@
 import {Injectable} from '@angular/core';
 import {Router} from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import 'rxjs/add/operator/map';
 import { Http, Headers, Response, RequestOptions, RequestMethod } from '@angular/http';
 
 @Injectable()
 export class UserprofileService {
     public token: string;
+    isLogged: boolean;
+
+    userChange: Subject<boolean> = new Subject<boolean>();
 
     constructor (private http: Http, private _router: Router){
         // set token if saved in local storage
         var loggedUser = JSON.parse(localStorage.getItem('loggedUser'));
         this.token = loggedUser && loggedUser.token;
+        this.isLogged = false;
     }
 
     login(email:string, password: string){
@@ -36,6 +40,8 @@ export class UserprofileService {
                         // store username and jwt token in local storage to keep user logged in between page refreshes
                         localStorage.setItem('id_token', token);
                         localStorage.setItem('loggedUser', JSON.stringify(data.user));
+                        this.isLogged = true;
+                        this.userChange.next(this.isLogged);
 
                         // return true for successful login
                         return true;
@@ -136,8 +142,9 @@ login_with_facebook(response){
     logout(){
         localStorage.removeItem("id_token");
         localStorage.removeItem("loggedUser");
+        this.isLogged = false;
+        this.userChange.next(this.isLogged);
         this._router.navigate(['home']);
-        window.location.reload();
     }
 }
 
