a568b57d1762ac4c363ff14956141ce8a06f1f78
Peter Odeny <odenypeter@gmail.com>
2017-08-01 13:53:30 +0300

    Define request options and headers globally to avoid redundancies

--- FrontEnd/src/app/pages/userprofile/userprofile.service.ts
+++ FrontEnd/src/app/pages/userprofile/userprofile.service.ts
@@ -4,6 +4,10 @@ import { Observable, Subject } from 'rxjs';
 import 'rxjs/add/operator/map';
 import { Http, Headers, Response, RequestOptions, RequestMethod } from '@angular/http';
 
+const TOLAPOFILE_USER_SERVER = 'http://127.0.0.1:8000/api/';
+const headers = new Headers({ 'Accept': 'application/json', 'Content-Type': 'application/json', });
+const options = new RequestOptions({ headers: headers, });
+
 @Injectable()
 export class UserprofileService {
     public token: string;
@@ -23,16 +27,8 @@ export class UserprofileService {
     }
 
     login(email:string, password: string){
-        let headers = new Headers({
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-         });
 
-         let options = new RequestOptions({
-            headers: headers,
-    });
-
-    return this.http.post('http://127.0.0.1:8000/api/auth/login/',JSON.stringify({ email: email, password: password}), options)
+         return this.http.post(TOLAPOFILE_USER_SERVER+'auth/login/', JSON.stringify({ email: email, password: password }), options)
         .map((response: Response) => {
                     // login successful if there's a jwt token in the response
                     let data = response.json();
@@ -58,18 +54,8 @@ export class UserprofileService {
     }
 
     register(userData){
-        console.log(JSON.stringify(userData));
-
-        let headers = new Headers({
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-         });
-
-         let options = new RequestOptions({
-            headers: headers,
-    });
-
-    return this.http.post('http://127.0.0.1:8000/api/auth/register/',JSON.stringify(userData), options)
+       
+        return this.http.post(TOLAPOFILE_USER_SERVER+'auth/register/', JSON.stringify(userData), options)
         .map((response: Response) => {
 
                     let data = response.json();
@@ -85,16 +71,7 @@ export class UserprofileService {
     //update user
     updateUser(user_id,userData){
 
-        let headers = new Headers({
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-         });
-
-         let options = new RequestOptions({
-            headers: headers,
-    });
-
-    return this.http.post('http://127.0.0.1:8000/api/tolausers/'+user_id+'/',JSON.stringify(userData), options)
+        return this.http.post(TOLAPOFILE_USER_SERVER+'/tolausers/'+ user_id + '/', JSON.stringify(userData), options)
         .map((response: Response) => {
 
                     let data = response.json();
@@ -110,16 +87,9 @@ export class UserprofileService {
 
 //login in wit facebook
 login_with_facebook(response){
-        let headers = new Headers({
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-         });
 
-         let options = new RequestOptions({
-            headers: headers,
-    });
-
-    return this.http.post('http://127.0.0.1:8000/api/auth/facebook/',JSON.stringify({"access_token": response.authResponse.accessToken,
+    return this.http.post(TOLAPOFILE_USER_SERVER+'/auth/facebook/', JSON.stringify({
+        "access_token": response.authResponse.accessToken,
         "backend": "facebook" }), options)
         .map((response: Response) => {
                     // login successful if there's a jwt token in the response
