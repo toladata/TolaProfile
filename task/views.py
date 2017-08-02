de228621deb5637ab0698ca23cf63ece46c5ddee
Peter Odeny <odenypeter@gmail.com>
2017-08-02 11:26:24 +0300

    Adjust the APIView query_set to return tasks created or assigned to the currently logged user

--- task/views.py
+++ task/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from rest_framework import viewsets
+from django.db.models import Q
 from rest_framework.permissions import IsAuthenticated
 from models import *
 from serializers import *
@@ -8,5 +9,13 @@ from serializers import *
 # Create your views here.
 class TaskListViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
-    queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the tasks
+        for the currently logged user.
+        """
+        user = self.request.user
+        task_object = Task.objects.filter(Q(created_by=user) | Q(assigned_to = user))
+        return task_object
